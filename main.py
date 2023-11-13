f97e91709c41260d2087af9588342577842586c3
Drownmale <slidy1337@gmail.com>
2023-11-13 20:17:46 +0100

    Update Aufgabenstellun

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ the first 10 terms will be:
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 https://projecteuler.net/problem=2
 """
-def print_fibonacci(length):
 
-    a = 1
-    b = 1
+
+def print_fibonacci(length):
+    i = 0
+    fib_list = [1, 2]
+    while True:
+        if i >= 8:
+            break
+        fib_list.append(fib_list[-2] + fib_list[-1])
+        i += 1
+    print(fib_list)
+
+
+
+print_fibonacci(2)
